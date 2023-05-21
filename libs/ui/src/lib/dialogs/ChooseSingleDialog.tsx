7ae89a6658d73b0212fba3d01759103dbc2dddd1
Mithardir <Mithardir@users.noreply.github.com>
2023-05-21 19:35:54 +0200

    added single dialog

---
+++ libs/ui/src/lib/dialogs/ChooseSingleDialog.tsx
@@ -0,0 +1,56 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  ListItemButton,
+  Typography,
+} from '@mui/material';
+
+export const ChooseSingleDialog = (props: {
+  title: string;
+  choices: Array<{
+    title: string;
+    image?: {
+      src: string;
+      width: number;
+      height: number;
+    };
+    action: () => void;
+  }>;
+}) => {
+  return (
+    <Dialog open={true} maxWidth="md">
+      <DialogTitle>{props.title}</DialogTitle>
+      <DialogContent>
+        <Grid container spacing={1}>
+          {props.choices.map((a, i) => (
+            <Grid key={i} item xs={!a.image ? 12 : 'auto'}>
+              <ListItemButton
+                key={a.title}
+                onClick={() => {
+                  a.action();
+                }}
+                style={{ width: 'auto' }}
+              >
+                {a.image ? (
+                  <img
+                    alt=""
+                    src={a.image.src}
+                    style={{
+                      width: a.image.width,
+                      height: a.image.height,
+                      position: 'relative',
+                    }}
+                  />
+                ) : (
+                  <Typography>{a.title}</Typography>
+                )}
+              </ListItemButton>
+            </Grid>
+          ))}
+        </Grid>
+      </DialogContent>
+    </Dialog>
+  );
+};
