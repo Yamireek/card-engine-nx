dec6f4c436db1cb5aca557531b545d921aca7061
Mithardir <Mithardir@users.noreply.github.com>
2023-05-22 11:24:21 +0200

    animations

--- libs/ui/src/lib/HandLayout.tsx
+++ libs/ui/src/lib/HandLayout.tsx
@@ -1,48 +1,50 @@
 import { CardDisplay, cardRatio } from "./CardDisplay";
+import { Flipped, Flipper } from "react-flip-toolkit";
 
 export const HandLayout = (props: { cardWidth: number; cards: number }) => {
   const cards = Array.from(Array(props.cards), (_, index) => index);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-evenly",
-        width: "100%",
-        maxWidth: cards.length * props.cardWidth + (cards.length - 1) * 8,
-        margin: "auto",
-        left: 0,
-        right: 0,
-      }}
-    >
-      {cards.map((c, i) => (
-        <div
-          key={c}
-          style={{
-            flex: cards.length - 1 === i ? "0 0 auto" : undefined,
-            overflowX: "hidden",
-            transitionProperty: "all",
-            transitionDuration: "1s",
-          }}
-        >
-          <CardDisplay
-            height={props.cardWidth / cardRatio}
-            image="https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Aragorn.jpg"
-            mark={{
-              attacking: true,
-              attacked: true,
-              defending: true,
-              questing: true,
-            }}
-            token={{
-              damage: 1,
-              progress: 1,
-              resources: 1,
-            }}
-            orientation="portrait"
-          />
-        </div>
-      ))}
-    </div>
+    <Flipper flipKey={cards.length}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          width: "100%",
+          maxWidth: cards.length * props.cardWidth + (cards.length - 1) * 8,
+          margin: "auto",
+          left: 0,
+          right: 0,
+        }}
+      >
+        {cards.map((c, i) => (
+          <Flipped key={c} flipId={c}>
+            <div
+              style={{
+                flex: cards.length - 1 === i ? "0 0 auto" : undefined,
+                overflowX: "hidden",
+              }}
+            >
+              <CardDisplay
+                height={props.cardWidth / cardRatio}
+                image="https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Aragorn.jpg"
+                mark={{
+                  attacking: true,
+                  attacked: true,
+                  defending: true,
+                  questing: true,
+                }}
+                token={{
+                  damage: 1,
+                  progress: 1,
+                  resources: 1,
+                }}
+                orientation="portrait"
+              />
+            </div>
+          </Flipped>
+        ))}
+      </div>
+    </Flipper>
   );
 };
