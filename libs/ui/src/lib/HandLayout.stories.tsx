b35f5fc866c482ae6003a352c1e092e18d5ed4fb
Pavel Ozogán <pavel.ozogan@softli.cz>
2023-05-22 15:37:23 +0200

    better props

--- libs/ui/src/lib/HandLayout.stories.tsx
+++ libs/ui/src/lib/HandLayout.stories.tsx
@@ -1,30 +1,67 @@
 import type { Meta } from "@storybook/react";
 import { HandLayout } from "./HandLayout";
 
+const options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const images = [
+  "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Aragorn.jpg",
+  "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Theodred.jpg",
+  "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Gloin.jpg",
+  "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Gimli.jpg",
+  "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Legolas.jpg",
+  "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Thalin.jpg",
+  "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Eowyn.jpg",
+  "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Eleanor.jpg",
+  "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Dunhere.jpg",
+  "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Denethor.jpg",
+  "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Glorfindel.jpg",
+  "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Beravor.jpg",
+];
+
 const Story: Meta<typeof HandLayout> = {
   component: HandLayout,
   title: "HandLayout",
   args: {
     cardWidth: 200,
-    cards: 7,
-    spread: 0.25,
-    rotate: 3,
+    rotate: 5,
+    cardImages: [
+      "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Aragorn.jpg",
+      "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Aragorn.jpg",
+      "https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Aragorn.jpg",
+    ],
   },
   argTypes: {
-    cards: {
-      control: { type: "range", min: 0, max: 15, step: 1 },
+    cardImages: {
+      options: options,
+      mapping: {
+        0: [],
+        1: images.slice(0, 1),
+        2: images.slice(0, 2),
+        3: images.slice(0, 3),
+        4: images.slice(0, 4),
+        5: images.slice(0, 5),
+        6: images.slice(0, 6),
+        7: images.slice(0, 7),
+        8: images.slice(0, 8),
+        9: images.slice(0, 9),
+        10: images.slice(0, 10),
+        11: images.slice(0, 11),
+        12: images.slice(0, 12),
+      },
+      control: {
+        label: "card amount",
+        type: "select",
+      },
     },
     cardWidth: {
       control: { type: "range", min: 100, max: 430, step: 1 },
-    },
-    spread: {
-      control: { type: "range", min: 0, max: 1, step: 0.01 },
     },
     rotate: {
       control: { type: "range", min: 0, max: 45, step: 0.1 },
     },
   },
 };
+
 export default Story;
 
 export const Primary = {
