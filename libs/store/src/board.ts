b6085f802c2c03242e91c24f5f3246fed65ccacb
Pavel Ozogán <pavel.ozogan@softli.cz>
2023-05-24 16:31:13 +0200

    .

--- libs/store/src/board.ts
+++ libs/store/src/board.ts
@@ -40,29 +40,41 @@ export class ZoneModel extends Model({
   orientation: prop<Orientation>(),
   cards: prop<CardModel[]>(),
 }) {
-  @computed
-  get cardScale(): number {
-    const calculated = calculateItemMaxItemSize(
-      this.size,
-      cardSize,
-      this.cards.length
-    );
+  // @computed
+  // get cardScale(): number {
+  //   const calculated = calculateItemMaxItemSize(
+  //     this.size,
+  //     cardSize,
+  //     this.cards.length
+  //   );
 
-    const scale = calculated.height / cardSize.height;
+  //   const scale = calculated.height / cardSize.height;
 
-    if (scale >= 1) {
-      return 1;
-    } else {
-      return scale;
-    }
-  }
+  //   if (scale >= 1) {
+  //     return 1;
+  //   } else {
+  //     return scale;
+  //   }
+  // }
 
   @computed
   get cardSlotSize(): Dimensions {
-    return {
-      height: this.cardScale * cardSize.height,
-      width: this.cardScale * cardSize.width,
+    const targetSize: Dimensions = {
+      width: cardSize.width * 1.2,
+      height: cardSize.height * 1.2,
     };
+
+    const calculated = calculateItemMaxItemSize(
+      this.size,
+      targetSize,
+      this.cards.length
+    );
+
+    if (calculated.height < targetSize.height) {
+      return calculated;
+    }
+
+    return targetSize;
   }
 }
 
@@ -110,21 +122,27 @@ export class CardModel extends Model({
 
   @computed
   get position(): Point3D {
-    const x = this.zone.size.width / this.zone.cardSlotSize.width;
-    const width = Math.floor(x);
+    const columns = Math.floor(
+      this.zone.size.width / this.zone.cardSlotSize.width
+    );
+
+    const spaceX = this.zone.cardSlotSize.width * 0.2;
+    const spaceY = this.zone.cardSlotSize.height * 0.2;
     return {
       x:
         this.zone.location.x +
-        this.zone.cardSlotSize.width * (this.index % width),
+        spaceX / 2 +
+        this.zone.cardSlotSize.width * (this.index % columns),
       y:
         this.zone.location.y +
-        this.zone.cardSlotSize.height * Math.floor(this.index / width),
+        spaceY / 2 +
+        this.zone.cardSlotSize.height * Math.floor(this.index / columns),
       z: 0,
     };
   }
 
   @computed
   get scale(): number {
-    return this.zone.cardScale;
+    return this.zone.cardSlotSize.height / cardSize.height / 1.2;
   }
 }
