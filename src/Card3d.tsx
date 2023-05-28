941e095d33e0858cb79ce10d046a3e44e8f27a2a
Mithardir <Mithardir@users.noreply.github.com>
2023-05-28 19:31:20 +0200

    card textures preloading

--- src/Card3d.tsx
+++ src/Card3d.tsx
@@ -1,22 +1,23 @@
 import { useTexture } from '@react-three/drei';
 import { image } from '@card-engine-nx/ui';
+import { Mesh, Texture } from 'three';
+import { useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
 //import { useSpring } from 'react-spring';
 
 export type Vector2 = [number, number];
 
 export type Vector3 = [number, number, number];
 
-export type CardImages = { front: string; back: string };
+export type CardTextures = { front: Texture; back: Texture };
 
 export type Card3dProps = {
   position: Vector3;
-  images: CardImages;
+  textures: CardTextures;
 };
 
 export const Card3d = (props: Card3dProps) => {
   const texture = useTexture({
-    front: props.images.front,
-    back: props.images.back,
     roughness: './textures/wood-2k/Wood026_2K_Roughness.png',
     normal: './textures/wood-2k/Wood026_2K_NormalGL.png',
   });
@@ -24,24 +25,50 @@ export const Card3d = (props: Card3dProps) => {
   return (
     <mesh position={props.position} rotation={[0, 0, 0]} castShadow>
       <boxGeometry args={[0.0635, 0.0889, 0.000305]} />
-      <meshPhysicalMaterial
-        map={texture.front}
-        roughnessMap={texture.roughness}
-        normalMap={texture.normal}
-      />
-      {/* <meshBasicMaterial attach="material-0" color="gray" />
+      <meshBasicMaterial attach="material-0" color="gray" />
       <meshBasicMaterial attach="material-1" color="gray" />
       <meshBasicMaterial attach="material-2" color="gray" />
       <meshBasicMaterial attach="material-3" color="gray" />
       <meshPhysicalMaterial
         attach="material-4"
-        map={texture.front}
+        map={props.textures.front}
         roughnessMap={texture.roughness}
         normalMap={texture.normal}
       />
       <meshPhysicalMaterial
         attach="material-5"
-        map={texture.back}
+        map={props.textures.back}
+        roughnessMap={texture.roughness}
+        normalMap={texture.normal}
+      />
+      {/* <meshPhysicalMaterial
+        map={props.textures.front}
+        roughnessMap={texture.roughness}
+        normalMap={texture.normal}
+      />
+      <meshBasicMaterial color="gray" />
+      <meshBasicMaterial color="gray" />
+      <meshBasicMaterial color="gray" />
+      <meshBasicMaterial color="gray" />
+      <meshPhysicalMaterial
+        map={props.textures.front}
+        roughnessMap={texture.roughness}
+        normalMap={texture.normal}
+      /> */}
+      {/* <meshPhysicalMaterial attach="material" />
+      <meshBasicMaterial attach="material-0" color="gray" />
+      <meshBasicMaterial attach="material-1" color="gray" />
+      <meshBasicMaterial attach="material-2" color="gray" />
+      <meshBasicMaterial attach="material-3" color="gray" />
+      <meshPhysicalMaterial
+        attach="material-4"
+        map={props.textures.front}
+        roughnessMap={texture.roughness}
+        normalMap={texture.normal}
+      />
+      <meshPhysicalMaterial
+        attach="material-5"
+        map={props.textures.back}
         roughnessMap={texture.roughness}
         normalMap={texture.normal}
       /> */}
