303751df8d5bfe8502ef1e60e4ac9aa99bf743a4
Mithardir <Mithardir@users.noreply.github.com>
2023-05-28 10:45:23 +0200

    text

--- src/Cylinder3d.tsx
+++ src/Cylinder3d.tsx
@@ -1,4 +1,4 @@
-import { useTexture } from '@react-three/drei';
+import { useTexture, Text } from '@react-three/drei';
 import { image } from '@card-engine-nx/ui';
 import { useState, useEffect } from 'react';
 import { animated, useSpring } from '@react-spring/three';
@@ -15,18 +15,18 @@ const Cylinder3d = (props: any) => {
   });
 
   const textureCard = useTexture({
-    map: image.test,
-  }); 
+    map: image.aragorn,
+  });
 
   const textureBoard = useTexture({
     map: image.board,
   });
 
-  useEffect(() => {
-    setInterval(() => {
-      setZ((p) => p + 100);
-    }, 1000);
-  }, []);
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     setZ((p) => p + 100);
+  //   }, 1000);
+  // }, []);
 
   return (
     <group>
@@ -34,6 +34,23 @@ const Cylinder3d = (props: any) => {
         <planeGeometry args={[4000, 4000]} />
         <meshPhysicalMaterial {...textureBoard} />
       </mesh>
+      <Text
+        color="black"
+        fontSize={14}
+        maxWidth={320}
+        lineHeight={1.15}
+        letterSpacing={0.01}
+        textAlign={'left'}
+        font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+        anchorX="left"
+        anchorY="top"
+        position={[-169, -146.5, 3]}
+        outlineWidth={6}
+        outlineColor="rgb(226,214,222)"
+      >
+        Response: After Aragorn commits to a quest, spend 1 resource from his
+        resource pool to ready him.
+      </Text>
       <animated.mesh
         onPointerEnter={() => {
           setZ(0);
@@ -45,7 +62,7 @@ const Cylinder3d = (props: any) => {
         rotation={[0, 0, 0]}
         castShadow
       >
-        <boxGeometry args={[256, 256, 2]} />
+        <boxGeometry args={[430, 600, 2]} />
         <meshPhysicalMaterial {...textureCard} />
       </animated.mesh>
     </group>
