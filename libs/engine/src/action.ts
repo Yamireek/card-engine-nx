8b1d3a72e3ca7645d553cf198058139cda03daac
Mithardir <Mithardir@users.noreply.github.com>
2023-06-03 09:06:34 +0200

    fixes

--- libs/engine/src/action.ts
+++ libs/engine/src/action.ts
@@ -1,8 +1,7 @@
 import {
   Action,
-  CardDefinition,
-  CardState,
   PlayerDeck,
+  PlayerState,
   Scenario,
   State,
   View,
@@ -11,13 +10,8 @@ import { getTargetPlayer } from './player/target';
 import { executePlayerAction } from './player/action';
 import { UIEvents } from './uiEvents';
 import { reverse, shuffle } from 'lodash/fp';
-import {
-  CardId,
-  GameZoneType,
-  PlayerId,
-  PlayerZoneType,
-  Side,
-} from '@card-engine-nx/basic';
+import { CardId, PlayerId } from '@card-engine-nx/basic';
+import { addPlayerCard, addGameCard, createPlayerState } from './utils';
 
 export type ExecutionContext = {
   state: State;
@@ -64,19 +58,7 @@ export function executeAction(action: Action, ctx: ExecutionContext) {
       ? 'D'
       : 'C';
 
-    ctx.state.players[playerId] = {
-      id: playerId,
-      thread: 0,
-      zones: {
-        hand: { cards: [], stack: false },
-        library: { cards: [], stack: true },
-        playerArea: { cards: [], stack: false },
-        discardPile: { cards: [], stack: true },
-        engaged: { cards: [], stack: false },
-      },
-      limitUses: { game: {} },
-      flags: {},
-    };
+    ctx.state.players[playerId] = createPlayerState(playerId);
 
     for (const hero of action.addPlayer.heroes) {
       addPlayerCard(ctx.state, hero, playerId, 'front', 'playerArea');
@@ -100,64 +82,6 @@ export function executeAction(action: Action, ctx: ExecutionContext) {
   }
 
   throw new Error(`unknown  action: ${JSON.stringify(action)}`);
-}
-
-function addPlayerCard(
-  state: State,
-  definition: CardDefinition,
-  owner: PlayerId,
-  side: Side,
-  zone: PlayerZoneType
-) {
-  const id = state.nextId;
-  state.cards[id] = createCardState(id, side, definition, owner);
-  state.players[owner]?.zones[zone].cards.push(id);
-  state.nextId++;
-}
-
-function addGameCard(
-  state: State,
-  definition: CardDefinition,
-  side: Side,
-  zone: GameZoneType
-) {
-  const id = state.nextId;
-  state.cards[id] = createCardState(id, side, definition, 'game');
-  state.zones[zone].cards.push(id);
-  state.nextId++;
-}
-
-function createCardState(
-  id: CardId,
-  side: Side,
-  definition: CardDefinition,
-  owner: PlayerId | 'game'
-): CardState {
-  return {
-    id,
-    token: {
-      damage: 0,
-      progress: 0,
-      resources: 0,
-    },
-    mark: {
-      questing: false,
-      attacking: false,
-      defending: false,
-      attacked: false,
-    },
-    sideUp: side,
-    tapped: false,
-    definition: definition,
-    attachments: [],
-    owner: owner,
-    controller: owner,
-    limitUses: {
-      phase: {},
-      round: {},
-    },
-    modifiers: [],
-  };
 }
 
 export function beginScenario(
