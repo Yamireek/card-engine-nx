cc19a803fd319b5cacd351bf155b51d622583bff
Mithardir <Mithardir@users.noreply.github.com>
2023-06-07 17:08:39 +0200

    .

--- src/StateContext.tsx
+++ src/StateContext.tsx
@@ -2,10 +2,15 @@ import { State, View } from '@card-engine-nx/state';
 import React, { useMemo } from 'react';
 import { createView } from '@card-engine-nx/engine';
 
+export type Moves = {
+  next: () => void;
+  makeChoice: (choice: number[]) => void;
+};
+
 export const StateContext = React.createContext<{
   state: State;
   view: View;
-  setState: (newState: State) => void;
+  moves: Moves;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 }>({} as any);
 
@@ -15,8 +20,17 @@ export const StateProvider = (
   const [state, setState] = React.useState<State>(props.init);
   const view = useMemo(() => createView(state), [state]);
 
+  const moves: Moves = {
+    next: () => {
+      throw new Error('not implemented');
+    },
+    makeChoice: () => {
+      throw new Error('not implemented');
+    },
+  };
+
   return (
-    <StateContext.Provider value={{ state, setState, view }}>
+    <StateContext.Provider value={{ state, moves, view }}>
       {props.children}
     </StateContext.Provider>
   );
