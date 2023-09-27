0c7547dce35e18f8494261760155564f64efbcb8
Mithardir <Mithardir@users.noreply.github.com>
2023-09-27 20:51:31 +0200

    play ally/attachment action

--- libs/engine/src/card/ability.ts
+++ libs/engine/src/card/ability.ts
@@ -2,6 +2,7 @@ import {
   Action,
   CardView,
   Modifier,
+  PaymentConditions,
   UserCardAction,
 } from '@card-engine-nx/state';
 import { applyModifier } from './modifier';
@@ -16,7 +17,109 @@ import {
 import { sequence } from '../utils/sequence';
 import { getTargetCards } from './target';
 import { calculateNumberExpr } from '../expr';
-import { createEventAction } from '../view';
+
+export function createPlayAllyAction(
+  self: CardView,
+  owner: PlayerId
+): Action[] {
+  const sphere = self.props.sphere;
+  const cost = self.props.cost;
+
+  if (!sphere || !cost) {
+    return [];
+  }
+
+  const payment: Action = {
+    player: {
+      target: owner,
+      action: { payResources: { amount: cost, sphere } },
+    },
+  };
+
+  const moveToPlay: Action = {
+    card: {
+      taget: self.id,
+      action: {
+        move: {
+          from: { owner, type: 'hand' },
+          to: { owner, type: 'playerArea' },
+          side: 'front',
+        },
+      },
+    },
+  };
+
+  return [
+    sequence(
+      { setCardVar: { name: 'self', value: self.id } },
+      { setPlayerVar: { name: 'controller', value: owner } },
+      sequence({ payment: { cost: payment, effect: moveToPlay } }),
+      { setPlayerVar: { name: 'controller', value: undefined } },
+      { setCardVar: { name: 'self', value: undefined } }
+    ),
+  ];
+}
+
+export function createPlayAttachmentAction(
+  self: CardView,
+  owner: PlayerId
+): Action[] {
+  const sphere = self.props.sphere;
+  const cost = self.props.cost;
+
+  if (!sphere || !cost || !self.attachesTo) {
+    return [];
+  }
+
+  const payment: Action = {
+    player: {
+      target: owner,
+      action: { payResources: { amount: cost, sphere } },
+    },
+  };
+
+  const attachTo: Action = {
+    player: {
+      target: owner,
+      action: {
+        chooseCardActions: {
+          title: 'Choose target for attachment',
+          multi: false,
+          optional: false,
+          target: self.attachesTo,
+          action: {
+            attachCard: self.id,
+          },
+        },
+      },
+    },
+  };
+
+  const moveToPlay: Action = {
+    card: {
+      taget: self.id,
+      action: {
+        move: {
+          from: { owner, type: 'hand' },
+          to: { owner, type: 'playerArea' },
+          side: 'front',
+        },
+      },
+    },
+  };
+
+  return [
+    sequence(
+      { setCardVar: { name: 'self', value: self.id } },
+      { setPlayerVar: { name: 'controller', value: owner } },
+      sequence({
+        payment: { cost: payment, effect: sequence(attachTo, moveToPlay) },
+      }),
+      { setPlayerVar: { name: 'controller', value: undefined } },
+      { setCardVar: { name: 'self', value: undefined } }
+    ),
+  ];
+}
 
 export function createCardActions(
   zone: PlayerZoneType | GameZoneType,
@@ -30,15 +133,8 @@ export function createCardActions(
     if (!ability.phase || ability.phase === phase) {
       const eventAction = createEventAction(
         self,
-        {
-          action,
-          card: self.id,
-          description: ability.description,
-          limit: ability.limit,
-          payment: ability.payment,
-          phase,
-        },
-        self.id,
+        ability.payment,
+        action,
         controller
       );
 
@@ -57,21 +153,21 @@ export function createCardActions(
     }
   }
 
-  // if (zone === 'hand' && card.props.type === 'ally') {
-  //   return createPlayAllyAction(card, self, controller).map((action) => ({
-  //     card: card.id,
-  //     description: `Play ally ${card.props.name}`,
-  //     action,
-  //   }));
-  // }
+  if (zone === 'hand' && self.props.type === 'ally') {
+    return createPlayAllyAction(self, controller).map((action) => ({
+      card: self.id,
+      description: `Play ally ${self.props.name}`,
+      action,
+    }));
+  }
 
-  // if (zone === 'hand' && card.props.type === 'attachment') {
-  //   return createPlayAttachmentAction(card, self, controller).map((action) => ({
-  //     card: card.id,
-  //     description: `Play attachment ${card.props.name}`,
-  //     action,
-  //   }));
-  // }
+  if (zone === 'hand' && self.props.type === 'attachment') {
+    return createPlayAttachmentAction(self, controller).map((action) => ({
+      card: self.id,
+      description: `Play attachment ${self.props.name}`,
+      action,
+    }));
+  }
 
   if (zone === 'playerArea') {
     return [
@@ -185,4 +281,45 @@ export function applyAbility(
   }
 
   throw new Error(`unknown ability: ${JSON.stringify(ability)}`);
+}
+export function createEventAction(
+  self: CardView,
+  conditions: PaymentConditions | undefined,
+  action: Action,
+  controller: PlayerId
+): Action | undefined {
+  const sphere = self.props.sphere;
+  const cost = self.props.cost;
+
+  if (!sphere || !cost) {
+    return;
+  }
+
+  const payment: Action = {
+    player: {
+      target: controller,
+      action: { payResources: { ...conditions, amount: cost, sphere } },
+    },
+  };
+
+  const discard: Action = {
+    card: {
+      taget: self.id,
+      action: {
+        move: {
+          from: { owner: controller, type: 'hand' },
+          to: { owner: controller, type: 'discardPile' },
+          side: 'front',
+        },
+      },
+    },
+  };
+
+  return sequence(
+    { setCardVar: { name: 'self', value: self.id } },
+    { setPlayerVar: { name: 'controller', value: controller } },
+    sequence({ payment: { cost: payment, effect: action } }, discard),
+    { setPlayerVar: { name: 'controller', value: undefined } },
+    { setCardVar: { name: 'self', value: undefined } }
+  );
 }
