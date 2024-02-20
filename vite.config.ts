2cb05f8b6ef132699d983196d9b9b6c9e90aa8f4
Mithardir <Mithardir@users.noreply.github.com>
2024-02-20 22:02:57 +0100

    better coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
-
+import { configDefaults } from 'vitest/config';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [viteTsConfigPaths()],
   test: {
     include: ['**/*.spec.ts'],
+    coverage: {
+      exclude: [
+        ...(configDefaults.coverage.exclude ?? []),
+        'libs/ui/*',
+        'apps/*',
+        '**/index.ts',
+      ],
+    },
   },
 });
